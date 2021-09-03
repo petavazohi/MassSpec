ef6d627b1fa9615af416862d48edab007b78838e
Pedram Tavadze <petavazohi@mix.wvu.edu>
2021-09-03 11:45:01 -0400

    Update live_view.py
    checkpoint

--- massspec/live_view.py
+++ massspec/live_view.py
@@ -15,44 +15,44 @@ from .raw_file import RawFile
 class LiveView(object):
     def __init__(self, path=".", only_new=True, delay=20):
         self.path = Path(path)
-        self.analyzed = []
         self.files = []
         self.dt = 0.5
         self.delay = delay
-        self.check_for_new()
         if only_new:
-            self.analyzed = self.to_analyze
+            self.analyzed = os.listdir(self.path.as_posix())
+        else:
+            self.analyzed
         self.count = 0
         self.init_plot()
-        self.run()
+        # self.run()
         # self.animation = Animation(self.fig, blit=True)
 
-
-
     def run(self):
-        check = self.check()
         try:
             while True:
-                to_analyze = check.next()
+                to_analyze = self.check()
                 if len(to_analyze) != 0:
                     for i in range(len(to_analyze), 0, -1):
-                        self.update(i*-1)
+                        self.update(self.count)
+                        self.count += 1
                         self.analyzed.append(to_analyze[i])
-                time.sleep(self.delay)
         except KeyboardInterrupt:
             print("Stopping now")
 
     def check(self):
+        time.sleep(self.delay)
         dirs = os.listdir(self.path.as_posix())
         new_files = np.setdiff1d(dirs, self.analyzed)
         to_analyze = []
         for ifile in new_files:
-            if len(ifile.split('.')) > 1:
+            if len(ifile.split(".")) > 1:
                 if ifile.split(".")[1] == "raw":
                     to_analyze.append(ifile)
                     self.files.append(
-                        RawFile(self.path.joinpath(ifile).as_posix()))
-        yield to_analyze
+                        RawFile(self.path.joinpath(ifile).as_posix())
+                    )
+        return to_analyze
+
 
     def init_plot(self):
         self.fig = plt.figure(figsize=(9, 7.5))
