27cb077483e0b70794a42ad7beaecd032d6c447c
Pedram Tavadze <petavazohi@mix.wvu.edu>
2021-09-03 10:40:16 -0400

    Update live_view.py
    switched to loop to a yield, next

--- massspec/live_view.py
+++ massspec/live_view.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import matplotlib.animation as animation
+from matplotlib.animation import Animation
 from pymsfilereader import MSFileReader
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import axes3d
@@ -16,28 +16,43 @@ class LiveView(object):
     def __init__(self, path=".", only_new=True, delay=20):
         self.path = Path(path)
         self.analyzed = []
-        self.to_analyze = []
         self.files = []
         self.dt = 0.5
+        self.delay = delay
         self.check_for_new()
-        if not only_new:
+        if only_new:
             self.analyzed = self.to_analyze
-
+        self.count = 0
         self.init_plot()
-        self.analyze()
+        self.run()
+        # self.animation = Animation(self.fig, blit=True)
 
-    def analyze(self):
-        for ifile in self.to_analyze:
-            self.files.append(RawFile(self.path.joinpath(ifile).as_posix()))
 
-    def check_for_new(self):
+
+    def run(self):
+        check = self.check()
+        try:
+            while True:
+                to_analyze = check.next()
+                if len(to_analyze) != 0:
+                    for i in range(len(to_analyze), 0, -1):
+                        self.update(i*-1)
+                        self.analyzed.append(to_analyze[i])
+                time.sleep(self.delay)
+        except KeyboardInterrupt:
+            print("Stopping now")
+
+    def check(self):
         dirs = os.listdir(self.path.as_posix())
         new_files = np.setdiff1d(dirs, self.analyzed)
+        to_analyze = []
         for ifile in new_files:
             if len(ifile.split('.')) > 1:
                 if ifile.split(".")[1] == "raw":
-                    self.to_analyze.append(ifile)
-        return
+                    to_analyze.append(ifile)
+                    self.files.append(
+                        RawFile(self.path.joinpath(ifile).as_posix()))
+        yield to_analyze
 
     def init_plot(self):
         self.fig = plt.figure(figsize=(9, 7.5))
@@ -49,7 +64,7 @@ class LiveView(object):
     def update(self, i):
         for ispectra in range(self.files[i].nspectra):
             x = self.files[i].data[ispectra][:, 0]
-            y = [self.files[i].dt * ispectra + i * self.dt] * len(x)
+            y = [self.files[i].dt * ispectra + self.count * self.dt] * len(x)
             z = self.files[i].data[ispectra][:, 1]
             self.ax.plot(x, y, z)
         plt.show()
