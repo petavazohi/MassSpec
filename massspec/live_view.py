0f3173dea85c328d39789139142775b55becd95c
Pedram Tavadze <petavazohi@mix.wvu.edu>
2021-09-06 18:36:21 -0400

    1st functional code

--- massspec/live_view.py
+++ massspec/live_view.py
@@ -1,41 +1,53 @@
 # -*- coding: utf-8 -*-
 
-from matplotlib.animation import Animation
+from matplotlib.animation import Animation, FuncAnimation
 from pymsfilereader import MSFileReader
 import matplotlib.pylab as plt
 from mpl_toolkits.mplot3d import axes3d
 import numpy as np
-from matplotlib import cm
 import os
 import time
 from pathlib import Path
 from .raw_file import RawFile
 
+colors = ['red', 'blue', 'green']
 
 class LiveView(object):
-    def __init__(self, path=".", only_new=True, delay=20):
+    def __init__(self, path=".", only_new=True, delay=20, run=True, mass_1=None, mass_2=None, spectrum_number=None, nrow=4, ncol=4):
+        plt.ion()
         self.path = Path(path)
         self.files = []
+        self.ratios = np.zeros(shape=(nrow, ncol))
+        self.ratios[:, :] = None
         self.dt = 0.5
         self.delay = delay
         if only_new:
             self.analyzed = os.listdir(self.path.as_posix())
         else:
-            self.analyzed
+            self.analyzed = []
         self.count = 0
+        self.nrow = nrow
+        self.ncol = ncol
+        self.mass_1 = mass_1
+        self.mass_2 = mass_2
+        self.spectrum_number = spectrum_number
         self.init_plot()
-        # self.run()
-        # self.animation = Animation(self.fig, blit=True)
+        # self.animation = Animation(self.fig, self.run, blit=True)
+        if run:
+            self.run()
 
+        
     def run(self):
         try:
             while True:
                 to_analyze = self.check()
                 if len(to_analyze) != 0:
-                    for i in range(len(to_analyze), 0, -1):
-                        self.update(self.count)
+                    for ifile in to_analyze:
+                        if ifile != "error":
+                            self.update(self.count)
+                            self.analyzed.append(ifile)
                         self.count += 1
-                        self.analyzed.append(to_analyze[i])
+                
         except KeyboardInterrupt:
             print("Stopping now")
 
@@ -47,24 +59,48 @@ class LiveView(object):
         for ifile in new_files:
             if len(ifile.split(".")) > 1:
                 if ifile.split(".")[1] == "raw":
-                    to_analyze.append(ifile)
-                    self.files.append(
-                        RawFile(self.path.joinpath(ifile).as_posix())
-                    )
+                    raw_file = RawFile(self.path.joinpath(ifile).as_posix())
+                    print("Analyzing {}".format(ifile))
+                    if not raw_file.has_error:
+                        to_analyze.append(ifile)
+                        self.files.append(raw_file)
+                        nfiles = len(self.files)
+                        irow = (nfiles-1)//self.ncol
+                        icol = (nfiles-1)%self.ncol
+                        self.ratios[irow, icol] = raw_file.get_ratio(self.mass_1, self.mass_2, self.spectrum_number)
+                    else :
+                        print("file {} has an error, skipping".format(ifile))
+                        self.analyzed.append(ifile)
+                        self.files.append(raw_file)
+                        to_analyze.append("error")
+
         return to_analyze
 
 
     def init_plot(self):
-        self.fig = plt.figure(figsize=(9, 7.5))
-        self.ax = self.fig.add_subplot(111, projection="3d")
-        self.ax.set_xlabel("m/z", fontsize=18)
-        self.ax.set_ylabel("Spectra Number", fontsize=18)
-        self.ax.set_zlabel("Intensity", fontsize=18)
+        self.fig = plt.figure(figsize=(16, 9))
+        self.ax_spectra = self.fig.add_subplot(121, projection="3d")
+        self.ax_spectra.view_init(elev=10, azim=60)
+        self.ax_spectra.set_xlabel("m/z", fontsize=18)
+        self.ax_spectra.set_ylabel("Spectra Number", fontsize=18)
+        self.ax_spectra.set_zlabel("Intensity", fontsize=18)
+
+        self.ax_drops = self.fig.add_subplot(122)
+        self.ax_drops.set_xlim(-0.5, self.ncol-0.5)
+        self.ax_drops.set_ylim(-0.5, self.nrow-0.5)
+        self.ax_drops.set_facecolor('red')
+        plt.show(block=False)
+        plt.draw()
 
     def update(self, i):
+        if (len(self.files)-1)%self.ncol == 0 :
+            self.ax_spectra.cla()
         for ispectra in range(self.files[i].nspectra):
-            x = self.files[i].data[ispectra][:, 0]
+            x = self.files[i].interpolated_data[ispectra][:, 0]
             y = [self.files[i].dt * ispectra + self.count * self.dt] * len(x)
-            z = self.files[i].data[ispectra][:, 1]
-            self.ax.plot(x, y, z)
-        plt.show()
+            z = self.files[i].interpolated_data[ispectra][:, 1]
+            self.ax_spectra.plot(x, y, z, color=colors[ispectra%3])
+        self.ax_drops.imshow(self.ratios,cmap='Greys')
+        self.fig.canvas.draw()
+        self.fig.canvas.flush_events()
+        # plt.close(self.fig)
--- massspec/raw_file.py
+++ massspec/raw_file.py
@@ -3,14 +3,19 @@
 from pymsfilereader import MSFileReader
 import numpy as np
 import matplotlib.pylab as plt
-
+from scipy.interpolate import interp1d
 
 class RawFile(object):  # need a better name
-    def __init__(self, filename):
+    def __init__(self, filename, interpolation='cubic', factor=2):
         self.filename = filename
         self.data = []
+        self.functions = []
+        self.interpolated_data = []
+        self.has_error=True
         self._get_data()
-
+        if not self.has_error:
+            self._get_interpolated(interpolation, factor)
+        
     def _get_data(self):
         try:
             ms_file = MSFileReader(self.filename)
@@ -20,11 +25,12 @@ class RawFile(object):  # need a better name
         if ms_file.GetNumSpectra() == 0:
             print("{} does not contain data".format(self.filename))
             return
-        for ispectra in range(1, ms_file.GetNumSpectra() + 1):
-            self.data.append(np.array(ms_file.GetMassListFromScanNum(ispectra)[0]).T)
+        for ispectrum in range(1, ms_file.GetNumSpectra() + 1):
+            self.data.append(np.array(ms_file.GetMassListFromScanNum(ispectrum)[0]).T)
         self._nspectra = ms_file.GetNumSpectra()
         self._dt = (ms_file.GetEndTime() - ms_file.GetStartTime()) / self._nspectra
         ms_file.Close()
+        self.has_error=False
 
     @property
     def nspectra(self):
@@ -34,6 +40,23 @@ class RawFile(object):  # need a better name
     def dt(self):
         return self._dt
 
+    def _get_interpolated(self, interpolation=None, factor=2):
+        if interpolation is None:
+            return 
+        else :
+            for ispectrum in range(self.nspectra):
+                self.functions.append(interp1d(self.data[ispectrum][:,0], self.data[ispectrum][:,1], kind=interpolation))
+                x = np.linspace(self.data[ispectrum][:, 0].min(), self.data[ispectrum][:, 0].max(), self.data[ispectrum].shape[0]*factor)
+                y = self.functions[ispectrum](x)
+                self.interpolated_data.append(np.array([x, y]).T)
+
+                
+    def get_ratio(self, mass_1, mass_2, spectrum_number):
+        return self.get_intensity(mass_1, spectrum_number)/self.get_intensity(mass_2, spectrum_number)
+
+    def get_intensity(self, mass, spectrum_number):
+        return self.functions[spectrum_number](mass)
+    
     def plot(self):
         plt.figure(figsize=(13, 9))
         for ispectrum in range(self.nspectra):
